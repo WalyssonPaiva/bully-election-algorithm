5f92e617401ccd46ff1709059039f496c69bc231
Walysson Paiva <walyssonpaiva@outlook.com>
2022-09-27 20:41:12 -0300

    chore: setup to client and server addresses

---
+++ const.py
@@ -0,0 +1,5 @@
+CHAT_SERVER_HOST = "localhost"
+CHAT_SERVER_PORT = 5001
+
+# IDs de usuários registrados com os respectivos endereços IP e números de porta
+registry = {"Alice":("localhost",5002, 1), "Bob":("localhost",5003, 2), "Charles": ("localhost", 5004, 3)}
